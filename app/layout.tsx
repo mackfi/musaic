9c66aaccd529dd57d4a7f828b8eb56a9963ea0d9
Mack Fischer <mackfisch@gmail.com>
2023-08-14 04:23:29 -0600

    Initialized multiple components and providers. Basic layout established, and Supabase has been connected. Currently working on user authentication.

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Figtree } from 'next/font/google'
+import {Sidebar} from '@/components/Sidebar'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
+import ModalProvider from '@/providers/ModalProvider'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'YouTube Music',
+  description: 'Listen to music!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider/>
+            <Sidebar>
+              {children}
+            </Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
+          
+        </body>
+    </html>
+  )
+}
