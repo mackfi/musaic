76a0d15e263ab2d3422ab4edb313d7ebb4f844b7
Mack Fischer <mackfisch@gmail.com>
2023-08-14 18:19:13 -0600

    Added basic user authentication that is connected via Supabase.

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,7 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -16,6 +17,7 @@ const Header: React.FC<HeaderProps> = (
     children,
     className
 ) => {
+    const authModal = useAuthModal();
     const router = useRouter();
 
     const handleLogout = () => {
@@ -116,7 +118,7 @@ const Header: React.FC<HeaderProps> = (
                     <>
                         <div>
                             <Button
-                            onClick={() =>  {}}
+                            onClick={authModal.onOpen}
                             className="
                                 font-medium
                                 px-6
