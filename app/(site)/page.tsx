9772e60c9301a6d1a5e914f748d26fd72ebf6ba1
Mack Fischer <mackfisch@gmail.com>
2023-08-24 10:10:45 -0600

    Added loading and error components across the site.

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,17 +3,11 @@ import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 import PageContent from "./components/PageContent";
 
-
-
-
-
-
-
-
 export const revalidate = 0;
 
 export default async function Home() {
   const songs = await getSongs();
+
   return (
     <div className="
       bg-neutral-900
